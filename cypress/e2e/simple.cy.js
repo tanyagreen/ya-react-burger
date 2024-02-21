e47beca1708fa57966d82b37781a5ba280894509
Tanya Karavaeva <kaktusgreen.kg@gmail.com>
2024-02-21 15:55:16 +0300

    spint-6/step-1

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -1,32 +1,38 @@
 beforeEach(() => {
     cy.intercept('GET', 'ingredients', { fixture: 'ingredients' });
-    cy.visit('http://localhost:3000');
+    cy.visit('/');
+
+    cy.get('ul li:first a').as('ingredientFirst');
+    cy.get('ul li:last a').as('ingredientLast');
+    cy.get('[data-testid="constructor"]').as('constructorContainer');
 });
 
 describe('Modal Ingredient works corectly', () => {
+    const modalOverlaySelector = '[data-testid="close-modal-overlay"]';
+
     it('should open modal', () => {
-        cy.get('ul li:first a').click();
+        cy.get('@ingredientFirst').click();
         cy.get('p.text_type_main-large').should('have.text', 'Детали ингридиента');
         cy.get('p.text_type_main-medium').should('have.text', 'Ингредиент 1');
     });
 
     it('should close modal by click on icon', () => {
-        cy.get('ul li:first a').click();
+        cy.get('@ingredientFirst').click();
         cy.get('[data-testid="close-modal-icon"]').click();
-        cy.get('[data-testid="close-modal-overlay"]').should('not.exist');
+        cy.get(modalOverlaySelector).should('not.exist');
     });
 
     it('should close modal by click on overlay', () => {
-        cy.get('ul li:first a').click();
-        cy.get('[data-testid="close-modal-overlay"]').click({ force: true });
-        cy.get('[data-testid="close-modal-overlay"]').should('not.exist');
+        cy.get('@ingredientFirst').click();
+        cy.get(modalOverlaySelector).click({ force: true });
+        cy.get(modalOverlaySelector).should('not.exist');
     });
 });
 
 describe('Dragging works corectly', () => {
     it('should drag element', () => {
-        cy.get('ul li:last a').trigger('dragstart');
-        cy.get('[data-testid="constructor"]').trigger('drop');
-        cy.get('[data-testid="constructor"]').contains('Ингредиент 2');
+        cy.get('@ingredientLast').trigger('dragstart');
+        cy.get('@constructorContainer').trigger('drop');
+        cy.get('@constructorContainer').contains('Ингредиент 2');
     });
 });
